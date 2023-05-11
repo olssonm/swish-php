1b3f4acc7762f279c23500d026d128f0979a9797
Marcus Olsson <hello@marcusolsson.me>
2023-05-11 09:23:48 +0200

    Improve testing

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,3 +3,11 @@
 namespace Olssonm\Swish\Test;
 
 uses(TestCase::class)->in(__DIR__);
+
+uses()->beforeEach(function () {
+    config()->set('swish.certificates', [
+        __DIR__ . '/certificates/root.pem',
+        __DIR__ . '/certificates/key.pem',
+        'swish',
+    ]);
+})->in(__DIR__);
