60fe8163e794f53e21df8eea79af056f3777d19a
Vincent Klaiber <vincent@doubledip.se>
2025-03-27 12:48:33 +0100

    Fetch storage from the container

--- src/Providers/SwishServiceProvider.php
+++ src/Providers/SwishServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Olssonm\Swish\Providers;
 
 use Illuminate\Contracts\Container\Container;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use Olssonm\Swish\Certificate;
 use Olssonm\Swish\Client;
@@ -19,14 +18,18 @@ class SwishServiceProvider extends ServiceProvider
         $this->mergeConfigFrom($source, 'swish');
 
         $this->app->singleton('swish', function (Container $app): Client {
+            /** @var \Illuminate\Config\Repository $config */
             $config = $app->get('config');
 
+            /** @var \Illuminate\Filesystem\FilesystemAdapter $storage */
+            $storage = $app->get('filesystem');
+
             $certificate = new Certificate(
-                clientPath: Storage::path($config['swish.certificates.client']),
+                clientPath: $storage->path($config['swish.certificates.client']),
                 passphrase: $config['swish.certificates.password'],
-                rootPath: Storage::path($config['swish.certificates.root']),
-                signingPath: Storage::path($config['swish.certificates.signing']),
-                signingPassphrase: $config['swish.certificates.signing_password'],
+                rootPath: $storage->path($config['swish.certificates.root']),
+                signingPath: $storage->path($config['swish.certificates.signing']),
+                signingPassphrase: $config['swish.certificates.signing_password']
             );
 
             return new Client($certificate, $config['swish.endpoint']);
