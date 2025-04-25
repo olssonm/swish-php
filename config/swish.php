6abcc67499fcee44d8ba847bd05c7838d780739f
Vincent Klaiber <vincent@doubledip.se>
2025-04-25 14:12:19 +0200

    Remove default values for root and signing certificate paths in configuration

--- config/swish.php
+++ config/swish.php
@@ -4,9 +4,9 @@ return [
     'certificates' => [
         'client' => env('SWISH_CLIENT_CERTIFICATE_PATH'),
         'password' => env('SWISH_CLIENT_CERTIFICATE_PASSWORD'),
-        'root' => env('SWISH_ROOT_CERTIFICATE_PATH', true),
-        'signing' => env('SWISH_SIGNING_CERTIFICATE_PATH', null), // Optional, used for payouts
-        'signing_password' => env('SWISH_SIGNING_CERTIFICATE_PASSWORD', null), // Optional, used for payouts
+        'root' => env('SWISH_ROOT_CERTIFICATE_PATH'),
+        'signing' => env('SWISH_SIGNING_CERTIFICATE_PATH'), // Optional, used for payouts
+        'signing_password' => env('SWISH_SIGNING_CERTIFICATE_PASSWORD'), // Optional, used for payouts
     ],
     'endpoint' => env('SWISH_URL', \Olssonm\Swish\Client::PRODUCTION_ENDPOINT),
 ];
